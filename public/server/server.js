ee8cbfdd7170c4d7503ec3b3e3aa56b4660a6040
Benjamin Cook <benjamincook1313@gmail.com>
2023-01-25 11:45:47 -0700

    express server

--- public/server/server.js
+++ public/server/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 
 
-const { PORT } = process.env
+const { PORT } = process.env;
 
-app.listen(PORT, () => console.log(`Server Listening on prot ${PORT}`);})
+app.listen(PORT, () => console.log(`Server Listening on port ${PORT}`));
