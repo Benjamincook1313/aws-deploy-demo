dfa4bf5dd263ccad2b3128fbd346e8755bed0e8b
Benjamin Cook <benjamincook1313@gmail.com>
2023-01-25 14:34:46 -0700

    update

--- public/server/server.js
+++ public/server/server.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-
+app.use(express.static(`${__dirname}/public`));
 
 const { PORT } = process.env;
 
-app.listen(PORT, () => console.log(`Server Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
