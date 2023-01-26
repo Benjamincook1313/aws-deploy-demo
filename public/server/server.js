29783426c325addb26a2167d4e94eac6d02020be
Benjamin Cook <benjamincook1313@gmail.com>
2023-01-25 17:24:08 -0700

    PORT variable

--- public/server/server.js
+++ public/server/server.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-// const { PORT } = process.env;
+const { PORT } = process.env;
 
-app.listen(5567, () => console.log(`Server listening on port 5567`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
